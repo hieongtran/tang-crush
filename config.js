b2475dab1a270d24e296caf5720473add0306494
hieongtran <98946892+hieongtran@users.noreply.github.com>
2022-02-03 16:35:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,11 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Haiii bạn đang đọc dòng này=33!',
+    introDesc: `Cọu nhớ phải làm nhoaaa
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    title: 'Cậu cs phải p3d3 hômmm',
+    desc: 'Chọn cho đúng vô bên cạnh tớ có khẩu AK đất :333',
+    btnYes: 'À...ừmmmmm. Yes 🤐',
+    btnNo: 'Ko phải đou, bro :3',
+    question:'Tại shao cậu lại gay dọoo',
+    btnReply: 'Gửi lại kết quả cho tớ <3',
+    
